2172c14fe0cdcf1a8ab83db7a45161ccc9a48b2d
DmitriyShutov1 <144882595+DmitriyShutov1@users.noreply.github.com>
2023-09-13 12:55:06 +0000

    1

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 
 #include "lib.h"
 
 using namespace std;
 
+int vowel_count(string s){
+    char vowel[] = {'a', 'o', 'e', 'i', 'u', 'y'};
+    int k = 0;
+    for(char elem: s){
+        if (count(begin(vowel), end(vowel), elem)){
+            k += 1;
+        }
+    }
+    return k;
+}
+
 int main() {
-    cout << "Bruh: " << endl
-         << A() << endl;
+    string s;
+    int b = 0;
+    while(cin >> s){
+        b += vowel_count(s);
+    }
+    cout << b;
 }
