0562ec13db83f60f386e806abe761633fafaae4e
DmitriyShutov1 <144882595+DmitriyShutov1@users.noreply.github.com>
2023-09-14 21:59:32 +0300

    Update hello_test.cpp

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -3,41 +3,39 @@
 #include <algorithm>
 #include "lib.h"
 
-using namespace std;
-
 TEST(vowel_count1, FirstTest) {
-  string input = "abcde fghi postav 10 pj";
+  std::string input = "abcde fghi postav 10 pj";
   int result = vowel_count(input);
   ASSERT_EQ(result, 5);
 }
 
 TEST(vowel_count1, SecondTest) {
-  string input = "";
+  std::string input = "";
   int result = vowel_count(input);
   ASSERT_EQ(result, 0);
 }
 
 TEST(vowel_count1, TretuiTest) {
-  string input = "    ";
+  std::string input = "    ";
   int result = vowel_count(input);
   ASSERT_EQ(result, 0);
 }
 
 
 TEST(vowel_count1, ChetvertiuTest) {
-  string input = "bnhghn hngh  hhhn [[ 73273 '],.";
+  std::string input = "bnhghn hngh  hhhn [[ 73273 '],.";
   int result = vowel_count(input);
   ASSERT_EQ(result, 0);
 }
 
 TEST(vowel_count1, PyatiuTest) {
-  string input = "aee yui ooyu";
+  std::string input = "aee yui ooyu";
   int result = vowel_count(input);
   ASSERT_EQ(result, 10);
 }
 
 TEST(vowel_count1, ShestoyTest) {
-  string input = "aeeh yuil ooyuk";
+  std::string input = "aeeh yuil ooyuk";
   int result = vowel_count(input);
   ASSERT_EQ(result, 10);
 }
