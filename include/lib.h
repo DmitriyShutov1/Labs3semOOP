83a362829878f0d40a913f50ff2ea2aa64d4633f
DmitriyShutov1 <144882595+DmitriyShutov1@users.noreply.github.com>
2023-10-07 10:21:18 +0300

    Update lib.h

--- include/lib.h
+++ include/lib.h
@@ -1,3 +1,30 @@
 #pragma once
 
-int A();
+class Money {
+private:
+    unsigned char* array;
+    int size = 0;
+public:
+    Money(double data);
+    Money(const Money& other);
+    void set_value(double data);
+    void print() const;
+    int get_size() const;
+    double get_value() const;
+    void serialization(std::string file_name);
+    void deserialization(std::string file_name);
+    unsigned char& operator[](int i) const;
+    Money operator+(Money& other) const;
+    Money operator-(Money& other) const;
+    Money operator+(double data) const;
+    Money operator-(double data) const;
+    bool operator<(Money& other) const;
+    bool operator<(double data) const;
+    bool operator>(Money& other) const;
+    bool operator>(double data) const;
+    bool operator==(Money& other) const;
+    bool operator==(double data) const;
+    Money& operator=(const Money& other);
+    Money& operator=(double data);
+    ~Money();
+};
