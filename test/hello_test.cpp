13f38e7154aeb406b825d10acf9aca8ba5d53e77
DmitriyShutov1 <144882595+DmitriyShutov1@users.noreply.github.com>
2023-10-07 12:20:19 +0100

    Update hello_test.cpp

--- test/hello_test.cpp
+++ test/hello_test.cpp
@@ -21,8 +21,40 @@ TEST_P(MoneyTest, ConstructorTest2) {
     EXPECT_EQ(second_money.get_value(), value);
 }
 
+TEST(SimpleTest, ConstructorTest3) {
+    std::string s = "123.561";
+    ASSERT_THROW({
+        Money second_money(s);
+    }, const char*);
+}
+
+TEST(SimpleTest, ConstructorTest4) {
+    std::string s = "123.56";
+    Money second_money(s);
+    EXPECT_EQ(second_money.get_value(), 123.56);
+}
+
+TEST(SimpleTest, ConstructorTest5) {
+    std::string s = "123.56";
+    Money second_money(s);
+    Money third_money(100);
+    second_money = std::move(third_money);
+    EXPECT_EQ(second_money.get_value(), 100);
+}
+
+TEST(SimpleTest, ConstructorTest6) {
+    Money second_money{"123.56"};
+    EXPECT_EQ(second_money.get_value(), 123.56);
+}
+
+TEST(SimpleTest, ConstructorTest7) {
+    ASSERT_THROW({
+        Money second_money{"123.561"};
+    }, const char*);
+}
+
 //ТЕСТ ОШИБОК
-TEST_P(MoneyTest, ConstructorTest3) {
+TEST_P(MoneyTest, ConstructorTest8) {
     double value = GetParam();
     ASSERT_THROW({
         Money second_money(value + 0.001);
@@ -50,7 +82,7 @@ TEST_P(MoneyTest, function_test) {
     EXPECT_EQ(value, first_money.get_value());
 }
 
-TEST(RegularTest, serialization_test) {
+TEST(SimpleTest, de_serialization_test) {
     Money test_money(10);
     test_money.serialization("file_in.txt");
     test_money.deserialization("file_out.txt");
@@ -59,7 +91,7 @@ TEST(RegularTest, serialization_test) {
 }
 
 //ТЕСТ ОПЕРАТОРОВ
-TEST_P(MoneyTest, operator_test) {
+TEST_P(MoneyTest, operator_test_1) {
     double value = GetParam();
     first_money = value;
     first_money = first_money + first_money;
@@ -68,18 +100,18 @@ TEST_P(MoneyTest, operator_test) {
     EXPECT_EQ(value*2, first_money.get_value());
 }
 
-TEST_P(MoneyTest, operator_next_test) {
+TEST_P(MoneyTest, operator_test_2) {
     double value = GetParam();
     EXPECT_EQ((first_money == value), (first_money < value*2));
 }
 
-TEST_P(MoneyTest, operator_next_test2) {
+TEST_P(MoneyTest, operator_test_3) {
     double value = GetParam();
     Money second_money(value*2);
     EXPECT_EQ((first_money < second_money), (first_money < second_money.get_value()));
 }
 
-TEST(Money_test, operator_next_test3) {
+TEST(SimpleTest, operator_test_4) {
     Money money(10.50);
 
     EXPECT_EQ(money[0], '1');
@@ -88,6 +120,7 @@ TEST(Money_test, operator_next_test3) {
     EXPECT_EQ(money[3], '0');
 }
 
-// Define the test parameters as a vector of values
+
+
 INSTANTIATE_TEST_CASE_P(MoneyTests, MoneyTest, ::testing::Values(
-    10.0, 20.5, 100.25)); // Add more values as needed
+    10.0, 20.5, 100.25)); 
