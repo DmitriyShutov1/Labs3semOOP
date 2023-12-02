485329667ec3f4af11520790899a2bcefda995a3
DmitriyShutov1 <144882595+DmitriyShutov1@users.noreply.github.com>
2023-12-02 04:49:56 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
+#include <vector>
 #include <iostream>
-
-#include "lib.h"
-
-using namespace std;
+#include "MyAllocator.h"
+#include "MyQueue.h"
+#include <deque>
 
 int main() {
-    cout << "Bruh: " << endl
-         << A() << endl;
+    MyQueue<int> queue;
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    
+    std::cout << "Queue size: " << queue.size() << std::endl;
+    std::cout << "Front element: " << queue.front() << std::endl;
+    std::cout << "Back element: " << queue.back() << std::endl;
+    
+
+
+    queue.push(10);
+    queue.push(20);
+    queue.push(30);
+    // Check begin and end methods
+    MyQueue<int>::iterator it = queue.begin();
+    MyQueue<int>::iterator endIt = queue.end();
+    while (it != endIt) {
+        std::cout << *it << " ";
+        ++it;
+    }
 }
+
+
